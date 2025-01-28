feac96c3a4b34c57c71532c11a1d0cc9321c07e5
Tejas Gadhave <tejas.gadhave@gramener.com>
2025-01-28 20:00:41 +0530

    corrected file name

--- api/index.py
+++ api/index.py
@@ -1,22 +1,23 @@
-from flask import Flask, request, jsonify
+# from flask import Flask, request, jsonify
 import json
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 # Example data: a dictionary mapping names to marks
-with open('std_marks.json') as f:
+with open('api/std_marks.json') as f:
     marks_data = json.load(f)
 
-@app.route('/api', methods=['GET'])
-def get_marks():
-    # Retrieve the list of names from query parameters
-    names = request.args.getlist('name')
+# @app.route('/api', methods=['GET'])
+# def get_marks():
+#     # Retrieve the list of names from query parameters
+#     names = request.args.getlist('name')
     
-    # Retrieve the corresponding marks from the marks_data dictionary
-    marks = [marks_data.get(name.lower(), None) for name in names]
+#     # Retrieve the corresponding marks from the marks_data dictionary
+#     marks = [marks_data.get(name.lower(), None) for name in names]
     
-    # Return the marks in a JSON response
-    return jsonify({'marks': marks})
+#     # Return the marks in a JSON response
+#     return jsonify({'marks': marks})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+print(marks_data)
