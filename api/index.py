5ef1f06612c3f32b27c1a8cf6663a4641a897e37
Tejas Gadhave <tejas.gadhave@gramener.com>
2025-01-28 19:50:40 +0530

    added marks json and changes to index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+# Example data: a dictionary mapping names to marks
+with open('std_marks.json') as f:
+    marks_data = json.load(f)
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/api', methods=['GET'])
+def get_marks():
+    # Retrieve the list of names from query parameters
+    names = request.args.getlist('name')
+    
+    # Retrieve the corresponding marks from the marks_data dictionary
+    marks = [marks_data.get(name.lower(), None) for name in names]
+    
+    # Return the marks in a JSON response
+    return jsonify({'marks': marks})
+
+if __name__ == '__main__':
+    app.run(debug=True)
